7fde848a9e253a4e9f41fc596127e62e0240540b
1622542 <1622542@Y352SDP11.ad.dundeeandangus.ac.uk>
2017-03-08 12:40:00 +0000

    Have added package and class for sound. Have added placeholder background music.

--- LittleFire/src/com/teamecho/game/Sound.java
+++ LittleFire/src/com/teamecho/game/Sound.java
@@ -5,10 +5,68 @@
  */
 package com.teamecho.game;
 
+import java.io.InputStream;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 /**
+ * Created by Jeff Grant 08/03/17
  *
  * @author 1622542
  */
+
+// Created a placeholder for background music. This can be swapped at a later stage for another track
+// No other sounds added at this time
+
 public class Sound {
+        
+    public static synchronized void play(InputStream soundResource)
+    {
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream ais = AudioSystem.getAudioInputStream(soundResource);
+                    clip.open(ais);
+                    clip.start();
+                }catch(Exception ex)
+                {
+                    System.out.println("Error playing sound " + ex.getMessage());
+                }
+            }
+        }).start();
+    }
     
+        public static synchronized void play(InputStream soundResource, boolean isMusic)
+    {
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                 Clip clip = AudioSystem.getClip();
+                 AudioInputStream ais = AudioSystem.getAudioInputStream(soundResource);
+                 clip.open(ais);
+                 clip.start();
+                 
+                 if(isMusic == true)
+                     clip.loop(Clip.LOOP_CONTINUOUSLY);
+                 
+                }catch(Exception ex)
+                {
+                    System.out.println("Error playing background sound" + ex.getMessage());
+                }
+            }
+        }).start();
+    }
+   
+        
+        
 }
