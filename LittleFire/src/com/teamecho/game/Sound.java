bc052675e9bcb318bc75b1a33eb3ff11f6d01b3f
1622542 <1622542@Y352SDP11.ad.dundeeandangus.ac.uk>
2017-03-08 12:05:39 +0000

    Have added package and class for sound.

---
+++ LittleFire/src/com/teamecho/game/Sound.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamecho.game;
+
+/**
+ *
+ * @author 1622542
+ */
+public class Sound {
+    
+}
